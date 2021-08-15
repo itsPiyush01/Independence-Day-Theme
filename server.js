715ab4465af481815f40af8b3aac36ecece6ce88
Piyush <piyushranjan1402@gmail.com>
2021-08-15 15:24:43 +0530

    add footer to index

--- server.js
+++ server.js
@@ -44,16 +44,27 @@ app.get('/', function (req, res) {
 app.get("/responses",(req,res)=>{
 
      let html=`
-     <h2>
-     <a href="/responses/fest">Fest</a>
-     </h2>
-     <h2>
-     <a href="/responses/fest">Fest</a>
-     </h2>
-     <h2>
-     <a href="/responses/fest">Fest</a>
-     </h2>
-     </br>
+     <html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>RESPONSES</title>
+</head>
+<body>
+<h2>
+<a href="/responses/fest">Fest</a>
+</h2>
+<h2>
+<a href="/responses/fest">Fest</a>
+</h2>
+<h2>
+<a href="/responses/fest">Fest</a>
+</h2>
+</br>
+</body>
+</html>
+    
      `
      res.write(html)
      res.send();
